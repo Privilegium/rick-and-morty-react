4d6f4cbc47126ef3719220ca457356eccfdb51ec
Sviatoslav <opryshkosm@icloud.com>
2023-03-22 13:50:22 +0200

    log in corrections vol.2

--- src/features/components/login/Login.jsx
+++ src/features/components/login/Login.jsx
@@ -1,19 +1,38 @@
 import { GoogleLogin } from '@react-oauth/google';
-
+import { useState, useEffect } from 'react';
 import './login.scss'
 
 const Login = () => {
+    const [isLogged, setIsLogged] = useState(false);
+
+    useEffect(() => {
+        const savedUserData = JSON.parse(localStorage.getItem('user'))
+        if (savedUserData) {
+            setIsLogged(true)
+        }
+    }, [])
+
+    const onLogOut = () => {
+        localStorage.removeItem('user')
+        setIsLogged(false)
+    }
 
     return (
         <div className="login-btn">
-            <GoogleLogin
+            {!isLogged && <GoogleLogin
                 onSuccess={credentialResponse => {
                     console.log(credentialResponse);
+                    setIsLogged(true)
+                    localStorage.setItem('user', JSON.stringify(credentialResponse))
                 }}
                 onError={() => {
                     console.log('Login Failed');
                 }}
-                />
+                />}
+            {isLogged && <div className='success'>
+                            <div className='success__text'>You are logged</div>
+                            <button className='success__btn' onClick={onLogOut}>Log out</button>
+                        </div>}
         </div>
     )
 }
